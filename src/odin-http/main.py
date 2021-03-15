7a459908627dd0ebf0fc1cb2d423525331fba882
dpressel <dpressel@gmail.com>
2021-03-15 00:18:41 -0400

    add FastAPI implementation
    The FastAPI version is intended to replace
    the current swagger_server version.  It is almost
    a drop-in replacement, though the authorization
    is slightly different.
    
    Changed the authorization client to work with
    either version, by trying to use a form first.
    
    Also make the ODIN_SCHEME an env var so
    we can get around having to constantly pass
    it.

---
+++ src/odin-http/main.py
@@ -0,0 +1,445 @@
+from typing import Optional, List, Dict
+import glob
+import requests
+import time
+import yaml
+import sys
+from fastapi import FastAPI, Depends, Body, Request
+from fastapi.middleware.cors import CORSMiddleware
+from kubernetes import client, config
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import asyncio
+import websockets
+import os
+import git
+from datetime import date, datetime
+import logging
+from eight_mile.utils import read_config_stream, read_yaml
+from odin.http.models import *
+from odin.http.orm import *
+from odin.http.utils import (
+    _convert_to_path,
+    set_repo_creds,
+    _request_status,
+    _submit_job,
+    _request_cleanup,
+    _request_data,
+    _request_events,
+    _validate_filename,
+)
+import jose.jwt as jwt
+# This indicates what branch we should be looking at in git for its pipelines
+PIPELINES_MAIN = os.environ.get('ODIN_PIPELINES_MAIN', 'master')
+JWT_ISSUER = os.environ.get('ODIN_AUTH_ISSUER', 'com.interactions')
+JWT_SECRET = os.environ.get('ODIN_SECRET')
+JWT_LIFETIME_SECONDS = os.environ.get('ODIN_TOKEN_DURATION', 60 * 60 * 12)
+JWT_ALGORITHM = os.environ.get('ODIN_AUTH_ALG', 'HS256')
+MIDGARD_PORT = os.environ.get('MIDGARD_PORT', 29999)
+MIDGARD_API_VERSION = os.environ.get('MIDGARD_API_VERSION', 'v1')
+LOGGER = logging.getLogger('odin-http')
+WS_SCHEME = os.environ.get('ODIN_WS_SCHEME', 'ws')
+WS_HOST = os.environ.get('ODIN_WS_HOST', 'localhost')
+WS_PORT = os.environ.get('ODIN_WS_PORT', 30000)
+ODIN_DB = os.getenv('ODIN_DB', 'odin_db')
+ODIN_FS_ROOT = os.getenv('ODIN_FS_ROOT', '/data/pipelines')
+LOGGER = logging.getLogger('odin-http')
+
+
+def get_db_config() -> Dict:
+    cred_params = {}
+    cred_params['backend'] = os.environ.get("ODIN_JOBS_BACKEND", "postgres")
+    cred_params['host'] = os.environ.get("SQL_HOST", "127.0.0.1")
+    cred_params['port'] = os.environ.get("DB_PORT", 5432)
+    cred_params['user'] = os.environ.get("DB_USER")
+    cred_params['passwd'] = os.environ.get("DB_PASS")
+    cred_params['odin_root_user'] = os.environ.get("ODIN_ROOT_USER")
+    cred_params['odin_root_passwd'] = os.environ.get("ODIN_ROOT_PASS")
+    cred_params['db'] = ODIN_DB
+    LOGGER.warning('%s %s %s %s', cred_params['user'], cred_params['db'], cred_params['host'], cred_params['port'])
+    return cred_params
+
+
+def get_ws_url():
+    return f'{WS_SCHEME}://{WS_HOST}:{WS_PORT}'
+
+class VersionedAPI(FastAPI):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.router.prefix = "/v1"
+
+
+app = VersionedAPI(prefix="/v1")
+
+# FIXME: tighten this up
+app.add_middleware(CORSMiddleware,
+                   allow_origins=['*'],
+                   allow_methods=['OPTIONS'],
+                   allow_headers=['Origin', 'X-Requested-With'])
+
+dao = Dao(dbname=ODIN_DB, **get_db_config())
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth")
+
+ws_event_loop = asyncio.new_event_loop()
+
+
+def _run_ws(fn):
+    asyncio.set_event_loop(ws_event_loop)
+    return asyncio.get_event_loop().run_until_complete(fn)
+
+
+def _read_file(file_to_read):
+    if not os.path.exists(file_to_read) or not os.path.isfile(file_to_read):
+        raise Exception("Invalid filename: {}".format(file_to_read))
+    with open(file_to_read, 'r') as rf:
+        return rf.read()
+
+
+def _get_job_loc(id_):
+    return os.path.join(ODIN_FS_ROOT, id_)
+
+
+def _get_job_file(id_, filename):
+    return os.path.join(_get_job_loc(id_), filename)
+
+
+def _get_job_repo_sha() -> str:
+    repo = git.Repo(ODIN_FS_ROOT)
+    repo = set_repo_creds(repo)
+    sha = repo.head.object.hexsha
+    return sha
+
+
+def _repo_version_match(repos: git.Repo) -> bool:
+    master_version = str(git.repo.fun.rev_parse(repos, f'origin/{PIPELINES_MAIN}'))
+    head_version = str(git.repo.fun.rev_parse(repos, 'HEAD'))
+    return master_version == head_version
+
+
+def _stash_pull_pop(repo: git.Repo):
+    do_push_pop = False
+    if repo.is_dirty():
+        do_push_pop = True
+        repo.git.stash()
+
+    if not _repo_version_match(repo):
+        repo.git.pull('--rebase', 'origin', f'{PIPELINES_MAIN}')
+
+    if do_push_pop:
+        repo.git.stash('pop')
+
+
+def _update_job_repo() -> None:
+    repo = git.Repo(ODIN_FS_ROOT)
+    repo = set_repo_creds(repo)
+    _stash_pull_pop(repo)
+
+
+def _add_to_job_repo(filename: str, message: str = None) -> str:
+    """Push a directory to git
+
+    :param dir: Directory
+    :raises ChoreSkippedException: If there is missing inputs.
+    :return: `True`
+    """
+
+    repo = git.Repo(ODIN_FS_ROOT)
+    repo = set_repo_creds(repo)
+    _stash_pull_pop(repo)
+    repo.git.add([filename])
+    if repo.is_dirty():
+        repo.git.commit(m=message)
+        repo.git.push()
+    sha = repo.head.object.hexsha
+    return sha
+
+
+@app.get("/app")
+def read_main(request: Request):
+    return {"message": "Hello World", "root_path": request.scope.get("root_path")}
+
+
+@app.get("/ping")
+def ping():
+    return "pong"
+
+
+@app.post("/auth")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> AuthResponseDefinition:
+    user: User = dao.get_user(form_data.username)
+    if not user:
+        raise Exception("Bad username!")
+    if not user.authenticate(form_data.password):
+        raise Exception("Bad password!")
+
+    timestamp = time.time()
+    payload = {
+        "iss": JWT_ISSUER,
+        "iat": int(timestamp),
+        "exp": int(timestamp + JWT_LIFETIME_SECONDS),
+        "sub": str(user.username),
+    }
+    message = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return AuthResponseDefinition(message=message)
+
+
+def _user_def(user: User) -> UserDefinition:
+    return UserDefinition(username=user.username, firstname=user.firstname, lastname=user.lastname)
+
+
+def _get_job_files(job_loc):
+    configs = []
+    job_dir = os.path.dirname(job_loc)
+    base = os.path.basename(job_loc)
+    for f in os.listdir(job_dir):
+        if f == base:
+            continue
+        if (
+            f.endswith(".py")
+            or f.endswith(".js")
+            or f.endswith("txt")
+            or f.endswith(".yml")
+            or f.endswith(".sh")
+            or f.endswith("yaml")
+            or f.endswith("json")
+            or f.endswith("config")
+            or f.endswith("cfg")
+        ):
+            id_ = os.path.join(job_dir, f)
+            content = _read_file(id_)
+            configs.append(ConfigDefinition(id=id_, name=f, content=content))
+    return configs
+
+
+def _job_def(id_):
+    try:
+        job_loc = _get_job_file(id_, 'main.yml')
+        object = read_yaml(job_loc)
+        job_def = JobDefinition(tasks=[])
+        job_def.location = job_loc
+        job_def.name = id_
+        job_def.id = id_
+        for t in object['tasks']:
+            if 'mounts' not in t:
+                t['mounts'] = t.pop('mount')
+            name = t['name']
+            t['id'] = f'{id_}--{name}'
+            task_def = TaskDefinition(**t)
+            job_def.tasks.append(task_def)
+        job_def.configs = _get_job_files(job_loc)
+        return job_def
+    except:
+        return None
+
+
+@app.get("/users")
+def get_users(q: Optional[str] = None) -> UserResults:
+    users = dao.get_users(q)
+    user_list = [_user_def(u) for u in users]
+    return UserResults(users=user_list)
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: str) -> UserWrapperDefinition:
+    user = dao.get_user(user_id)
+    user_def = _user_def(user)
+    return UserWrapperDefinition(user=user_def)
+
+
+@app.post("/users")
+def create_user(user_def: UserWrapperDefinition, token: str=Depends(oauth2_scheme)) -> UserWrapperDefinition:
+    user = dao.create_user(user_def)
+    user_def = _user_def(user)
+    return UserWrapperDefinition(user=user_def)
+
+
+@app.put("/users/{user_id}")
+def update_user(user_id: str, user_def: UserDefinition, token: str=Depends(oauth2_scheme)) -> UserWrapperDefinition:
+    if user_id != user_def.username:
+        LOGGER.warning("%s vs %s", user_id, user_def.username)
+        user_def.username = user_id
+    user = dao.update_user(user_def)
+    user_def = _user_def(user)
+    return UserWrapperDefinition(user=user_def)
+
+
+@app.delete("/users/{user_id}")
+def update_user(user_id: str, token: str=Depends(oauth2_scheme)) -> UserWrapperDefinition:
+    user = dao.delete_user(user_id)
+    user_def = _user_def(user)
+    return UserWrapperDefinition(user=user_def)
+
+
+@app.get("/nodes")
+def get_node_info() -> str:
+    def get_nodes() -> List[str]:
+        try:
+            config.load_incluster_config()
+        except config.config_exception.ConfigException:
+            config.load_kube_config()
+
+        core_api = client.CoreV1Api()
+        return core_api.list_node().items
+    results = []
+    for node in get_nodes():
+        addresses = node.status.addresses
+        internal_address = [a.address for a in addresses if a.type == 'InternalIP'][0]
+        host = [a.address for a in addresses if a.type == 'Hostname'][0]
+        capacity_dict = node.status.capacity
+        midgard_url = f'http://{internal_address}:{MIDGARD_PORT}/{MIDGARD_API_VERSION}/gpus'
+        gpu_infos = requests.get(midgard_url).json()['gpus']
+        capacity_dict['gpus'] = gpu_infos
+        capacity_dict['host'] = host
+        capacity_dict['internalIP'] = internal_address
+        results.append(capacity_dict)
+
+    return {'nodes': results}
+
+
+@app.get("/pipelines")
+def get_pipelines(q: Optional[str] = None) -> PipelineResults:
+    loop = ws_event_loop
+    asyncio.set_event_loop(loop)
+
+    s = asyncio.get_event_loop().run_until_complete(_request_status(get_ws_url(), q))
+    return s
+
+@app.post("/pipelines")
+def create_pipeline(pipe_def: PipelineWrapperDefinition, token: str=Depends(oauth2_scheme)) -> PipelineWrapperDefinition:
+    job = _convert_to_path(pipe_def.pipeline.job)
+    _update_job_repo()
+    pipe_id = _run_ws(_submit_job(get_ws_url(), job))
+    p = PipelineDefinition(name=pipe_id, id=pipe_id, job=job)
+    return PipelineWrapperDefinition(pipeline=p)
+
+
+@app.get("/pipelines/{pipe_id}")
+def get_pipeline(pipe_id: str) -> PipelineWrapperDefinition:
+    result = _run_ws(_request_status(get_ws_url(), pipe_id))
+    if result:
+        return PipelineWrapperDefinition(pipeline=result.pipelines[0])
+    raise Exception(f"No such pipeline {pipe_id}")
+
+
+@app.delete("/pipelines/{pipe_id}")
+def cleanup_pipeline(pipe_id: str, db: bool=False, fs: bool=False, token: str=Depends(oauth2_scheme)) -> PipelineCleanupResults:
+    cleanup_info = _run_ws(_request_cleanup(get_ws_url(), pipe_id, db, fs))
+    return cleanup_info
+
+
+@app.get("/resources/{resource_id}/events")
+def get_events(resource_id) -> EventResults:
+    event_info = _run_ws(_request_events(get_ws_url(), resource_id))
+    return event_info
+
+
+@app.get("/resources/{resource_id}/data")
+def get_data(resource_id):
+    data_info = _run_ws(_request_data(get_ws_url(), resource_id))
+    return data_info
+
+@app.get("/jobs")
+def get_jobs(q: Optional[str] = '*') -> JobResults:
+    _update_job_repo()
+    job_defs = []
+    q = _convert_to_path(q)
+    file_star = '{}*'.format(q)
+    for path_value in glob.glob(os.path.join(ODIN_FS_ROOT, file_star)):
+        id_ = os.path.basename(path_value)
+        job_loc = os.path.join(path_value, 'main.yml')
+        if os.path.exists(job_loc) and os.path.isfile(job_loc):
+            job_def = _job_def(id_)
+            if job_def:
+                job_defs.append(job_def)
+    return JobResults(job_defs)
+
+
+@app.get("/jobs/{job_id}")
+def get_job(job_id: str) -> JobWrapperDefinition:
+    id_ = _convert_to_path(job_id)
+    _update_job_repo()
+    return JobWrapperDefinition(_job_def(id_))
+
+
+@app.post("/jobs")
+def create_job(job_def: JobWrapperDefinition, token: str=Depends(oauth2_scheme)) -> JobWrapperDefinition:
+    id_ = _convert_to_path(job_def.job.name)
+    new_job_path = _get_job_loc(id_)
+    if os.path.exists(new_job_path):
+        raise Exception(f"There is already a job at {id}")
+    os.makedirs(new_job_path)
+    file_to_write = _get_job_file(id_, 'main.yml')
+    task_obj = {'tasks': []}
+    with open(file_to_write, 'w') as wf:
+        yaml.dump(task_obj, wf)
+    sha = _add_to_job_repo(file_to_write, "via odin-http create_job")
+    LOGGER.info(f'Updated git {sha}')
+
+    updated_job_def = JobDefinition(id=id_,
+                                    name=id_,
+                                    location=new_job_path,
+                                    creation_time=datetime.datetime.now())
+    return JobWrapperDefinition(job_def=updated_job_def)
+
+
+def _to_dict(jid, task):
+    """User can put in whatever task name they want, but we will rewrite it"""
+    task = task.to_dict()
+    task_name = task.get('name')
+    task['id'] = f'{jid}--{task_name}'
+    return task
+
+
+@app.put("/jobs/{job_id}")
+def update_job(job_id: str, job_def: JobDefinition, token: str=Depends(oauth2_scheme)) -> JobWrapperDefinition:
+    id_ = _convert_to_path(job_id)
+    tasks = [_to_dict(id_, s) for s in job_def.tasks]
+    # compute the id of the tasks if needed
+
+    task_obj = {'tasks': tasks}
+    configs = job_def.configs
+    for config in configs:
+        file_to_write = _get_job_file(id_, config.name)
+        with open(file_to_write, 'w') as wf:
+            wf.write(config.content)
+        _add_to_job_repo(file_to_write, 'via odin-http create_job')
+
+    job_loc = _get_job_loc(id_)
+    if os.path.exists(job_loc) is False:
+        logging.info('Creating job location {}'.format(job_loc))
+        os.makedirs(job_loc)
+
+    file_to_write = _get_job_file(id_, 'main.yml')
+    with open(file_to_write, 'w') as wf:
+        yaml.dump(task_obj, wf)
+    sha = _add_to_job_repo(file_to_write, "via odin-http create_job")
+    LOGGER.info(f'Updated git {sha}')
+    job_def.tasks = tasks
+    return JobWrapperDefinition(job_def)
+
+
+@app.get("/jobs/{job_id}/files/{filename}")
+def download_job_file(job_id: str, filename: str) -> str:
+    id_ = _convert_to_path(job_id)
+    _update_job_repo()
+    return _read_file(_get_job_file(id_, filename))
+
+
+@app.post("/jobs/{job_id}/files/{filename}")
+def upload_job_file(job_id: str, filename: str, body: Request=Body(..., media_type="application/binary"), token: str=Depends(oauth2_scheme)):
+    id_ = _convert_to_path(job_id)
+    _validate_filename(filename)
+    file_to_write = _get_job_file(id_, filename)
+
+    if os.path.exists(file_to_write):
+        logging.warning("Found {}.  Overwriting".format(body))
+    with open(file_to_write, 'wb') as wf:
+        wf.write(body)
+
+    sha = _add_to_job_repo(file_to_write, "via odin-http upload_job_file")
+    ud = UploadDefinition()
+    ud.location = f'{file_to_write}@{sha}'
+    ud.bytes = os.stat(file_to_write).st_size
+    return ud
+
+
+
---
+++ src/odin-http/odin/http/utils.py
@@ -0,0 +1,208 @@
+import os
+import logging
+import websockets
+import git
+import json
+import re
+from odin.http.models import *
+
+LOGGER = logging.getLogger('odin-http')
+
+
+class APIField:
+    """Keys that we use when communicating between server and clients."""
+
+    STATUS = 'status'
+    RESPONSE = 'response'
+    COMMAND = 'command'
+    REQUEST = 'request'
+
+
+class APIStatus:
+    """Status codes used between the server and client."""
+
+    OK = 'OK'
+    ERROR = 'ERROR'
+    END = 'END'
+
+
+def _event_def(id_, row):
+    event_def = EventDefinition(
+        id=id_,
+        event_type=row['type'],
+        reason=row['reason'],
+        source=row['source'],
+        message=row['message'],
+        timestamp=row['timestamp'],
+    )
+
+    return event_def
+
+
+def _step_status(row):
+    s = TaskStatusDefinition(
+        task=row['task'],
+        status=row['status'],
+        command=row['command'],
+        name=row['name'],
+        image=row['image'],
+        resource_type=row['resource_type'],
+        submit_time=row['submitted'],
+        completion_time=row['completed'],
+        id=row['task'],
+        resource_id=row['task']
+    )
+
+    return s
+
+
+def _pipeline_status(row):
+    p = PipelineDefinition(
+        name=row['label'],
+        id=row['label'],
+        job=row['job'],
+        status=row['status'],
+        submit_time=row['submitted'],
+        version=row.get('version'),
+        completion_time=row['completed']
+    )
+    return p
+
+
+def _convert_to_path(resource_id: str) -> str:
+    """Convert something like dpressel__ag-news into dpressel/ag-news
+    :param resource_id:
+    :return:
+    """
+    return resource_id.replace('__', '/')
+
+
+async def _submit_job(ws, work) -> None:
+    _pipe_id = None
+    async with websockets.connect(ws) as websocket:
+        await websocket.send(json.dumps({APIField.COMMAND: 'START', APIField.REQUEST: work}))
+        result = json.loads(await websocket.recv())
+        if result[APIField.STATUS] == APIStatus.ERROR:
+            LOGGER.error(result)
+            raise Exception("Invalid response")
+
+        _pipe_id = result[APIField.RESPONSE]
+        return _pipe_id
+
+
+async def _request_status(ws, work):
+    """Request the status of a job from the server."""
+    pipe_statuses = []
+    async with websockets.connect(ws) as websocket:
+        await websocket.send(json.dumps({APIField.COMMAND: 'STATUS', APIField.REQUEST: work}))
+
+        results = json.loads(await websocket.recv())
+        if results[APIField.STATUS] == APIStatus.ERROR:
+            LOGGER.error(results)
+            return
+        if results[APIField.STATUS] == APIStatus.OK:
+            results = results[APIField.RESPONSE]
+            for result in results:
+                task_status = [_step_status(r) for r in result['task_statuses']]
+                pipe_status = _pipeline_status(result['pipeline_status'])
+                pipe_status.tasks = task_status
+                pipe_statuses.append(pipe_status)
+        return PipelineResults(pipelines=pipe_statuses)
+
+
+async def _request_cleanup(ws: str, work: str, purge_db: bool = False, purge_fs: bool = False):
+    """Request the work is cleaned up by the server."""
+    async with websockets.connect(ws) as websocket:
+        args = {'work': work, 'purge_db': purge_db, 'purge_fs': purge_fs}
+        await websocket.send(json.dumps({APIField.COMMAND: 'CLEANUP', APIField.REQUEST: args}))
+
+        results = json.loads(await websocket.recv())
+        if results[APIField.STATUS] == APIStatus.ERROR:
+            LOGGER.error(results)
+            return []
+        if results[APIField.STATUS] == APIStatus.OK:
+            cleaned = results[APIField.RESPONSE]
+            cleanup_def = [PipelineCleanupDefinition(**c) for c in cleaned]
+            return PipelineCleanupResults(cleanups=cleanup_def)
+
+
+async def _request_events(ws: str, resource: str, namespace: str = 'default', kind: str = 'Pod') -> None:
+    """Get k8s events for some resource.
+
+    :param uri: The location of the server
+    :param pod: The name of the resource you are asking about.
+    :param namespace: The namespace of the resource you are asking about.
+    :param kind: The kind of resource you are asking about.
+    """
+    async with websockets.connect(ws) as websocket:
+        await websocket.send(
+            json.dumps(
+                {
+                    APIField.COMMAND: 'EVENTS',
+                    APIField.REQUEST: {'resource': resource, 'namespace': namespace, 'kind': kind},
+                }
+            )
+        )
+        resp = json.loads(await websocket.recv())
+        if resp[APIField.STATUS] == APIStatus.ERROR:
+            LOGGER.error(resp)
+            return
+        if resp[APIField.STATUS] == APIStatus.OK:
+            _events = [_event_def(f'evt-{resource}-{i}', r) for i, r in enumerate(resp[APIField.RESPONSE])]
+
+        return EventResults(events=_events)
+
+
+async def _request_data(ws: str, resource: str, namespace: str = 'default') -> None:
+    """Get data for resource as contained in `jobs_db`
+
+    :param uri: The location of the server
+    :param pod: The name of the resource you are asking about.
+    :param namespace: The namespace of the resource you are asking about.
+    :param kind: The kind of resource you are asking about.
+    """
+    async with websockets.connect(ws) as websocket:
+        await websocket.send(
+            json.dumps(
+                {
+                    APIField.COMMAND: 'DATA',
+                    APIField.REQUEST: {'resource': resource, 'namespace': namespace},
+                }
+            )
+        )
+        resp = json.loads(await websocket.recv())
+        if resp[APIField.STATUS] == APIStatus.ERROR:
+            LOGGER.error(resp)
+            return
+        if resp[APIField.STATUS] == APIStatus.OK:
+            return resp[APIField.RESPONSE]
+
+def _validate_filename(filename):
+    matcher = re.compile(r'^[^<>:;,?"*|/]+$')
+    if not matcher.match(filename):
+        raise Exception("Invalid file name: {}".format())
+
+
+def set_repo_creds(
+    repo: git.Repo,
+    name: Optional[str] = None,
+    email: Optional[str] = None,
+    name_env: str = "ODIN_GIT_NAME",
+    email_env: str = "ODIN_GIT_EMAIL"
+) -> git.Repo:
+    """Set the name and email on the git repo so you can commit.
+
+    :param repo: The repo object
+    :param name: The name to set
+    :param email: The email to set
+    :param name_env: The env variable to read the name from if no name provided
+    :param email_env: The env variable to read the email from if no email is provided
+
+    :returns: The repo with the creds set
+    """
+    name = os.environ[name_env] if name is None else name
+    email = os.environ[email_env] if email is None else email
+    repo.config_writer().set_value("user", "name", name).release()
+    repo.config_writer().set_value("user", "email", email).release()
+    return repo
+
