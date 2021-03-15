affb4a268761eff8f9f28ff59f3df14d15006425
dpressel <dpressel@gmail.com>
2021-03-15 10:21:27 -0400

    update the setup.py to be fastAPI

--- src/odin-http/setup.py
+++ src/odin-http/setup.py
@@ -12,22 +12,34 @@ VERSION = "1.0.0"
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 
-REQUIRES = ["connexion"]
+REQUIRES = [
+    "fastapi",
+    "fastapi_camelcase",
+    "uvicorn[standard]",
+    "websockets",
+    "numpy",
+    "pyyaml",
+    "GitPython",
+    "requests",
+    "kubernetes",
+    "requests-async",
+    "python-jose",
+    "bcrypt",
+    "SQLAlchemy",
+    "psycopg2-binary",
+]
+
 
 setup(
     name=NAME,
     version=VERSION,
     description="odin API",
-    author_email="mead.baseline@gmail.com",
+    author_email="odin@interactions.com",
     url="",
-    keywords=["Swagger", "odin API"],
+    keywords=["FastAPI", "odin API"],
     install_requires=REQUIRES,
     packages=find_packages(),
-    package_data={'': ['specs/odin.yaml']},
-    include_package_data=True,
-    entry_points={
-        'console_scripts': ['odin-http=swagger_server.__main__:main']},
     long_description="""\
-    This spec defines the odin API, which is used to communicate with the odin server.
+    odin HTTP API which is used to communicate with the odin server.
     """
 )
