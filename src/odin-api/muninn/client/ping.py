8551fb2ece57efafeed0a6898e808b5f5e85c55f
dpressel <dpressel@gmail.com>
2022-03-04 14:14:49 -0500

    change odin-ping to use /v1/app endpoint

--- src/odin-api/muninn/client/ping.py
+++ src/odin-api/muninn/client/ping.py
@@ -5,24 +5,37 @@ import asyncio
 from datetime import datetime
 import json
 import websockets
-from muninn import ODIN_URL, ODIN_PORT, ODIN_SCHEME, ODIN_API_LOGGER, APIField, APIStatus
+from muninn import ODIN_URL, ODIN_PORT, ODIN_SCHEME, HttpClient, ODIN_API_LOGGER, APIField, APIStatus
 
 
-async def ping(uri: str, message: str) -> None:
+async def ping(uri: str) -> None:
     """Ping odin at uri and send message.
 
     :param uri: The location of the server
-    :param message: The message you expect to see back
     :raises RuntimeError: If the server returns an error
     """
     async with websockets.connect(uri) as websocket:
+        message = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
         await websocket.send(json.dumps({APIField.COMMAND: 'PING', APIField.REQUEST: message}))
         resp = json.loads(await websocket.recv())
         if resp[APIField.STATUS] == APIStatus.ERROR:
-            ODIN_API_LOGGER.error(resp)
+            print('ERROR', resp)
             raise RuntimeError(resp)
-        ODIN_API_LOGGER.info(resp[APIField.RESPONSE])
+        print(resp[APIField.RESPONSE])
 
+def ping_http(url: str) -> None:
+    """Get data for a resource over HTTP
+
+    :param url: The base URL
+    :param resource: The resource ID
+    """
+    try:
+
+        results = HttpClient(url).app_info()
+        print(json.dumps(results))
+    except Exception as e:
+        print('Error pinging server')
+        print(e)
 
 def main():
     """Websocket client for pinging odin."""
@@ -31,14 +44,20 @@ def main():
     parser.add_argument('--port', default=ODIN_PORT)
     parser.add_argument(
         '--scheme',
-        choices={'wss', 'ws'},
+        choices={'wss', 'ws', 'http', 'https'},
         default=ODIN_SCHEME,
-        help='Websocket connection protocol, use `wss` for remote connections and `ws` for localhost',
+        help='Connection protocol, use `http` for REST, use `wss` for remote connections and `ws` for localhost',
     )
-    parser.add_argument('--message', default=datetime.now().strftime("%Y-%m-%dT%H:%M:%S"))
+    args = parser.parse_args()
+    url = f'{args.scheme}://{args.host}:{args.port}'
+
+    if args.scheme.startswith('ws'):
+        asyncio.get_event_loop().run_until_complete(ping(url))
+    else:
+        ping_http(url)
     args = parser.parse_args()
     ws = f'{args.scheme}://{args.host}:{args.port}'
-    asyncio.get_event_loop().run_until_complete(ping(ws, args.message))
+
 
 
 if __name__ == "__main__":
